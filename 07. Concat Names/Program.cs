6ddbb170977b3fe27429b92f1793caceb464e538
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-25 02:45:00 +0300

    Add files via upload

---
+++ 07. Concat Names/Program.cs
@@ -0,0 +1,6 @@
+﻿string firstName = Console.ReadLine();
+string lastName = Console.ReadLine();
+string delimiter = Console.ReadLine();
+string result = firstName + delimiter + lastName;
+
+Console.WriteLine(result);
