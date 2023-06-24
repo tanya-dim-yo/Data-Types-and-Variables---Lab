387e658b890fb4ab63b6422b1e004039768dbcf1
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-25 02:44:18 +0300

    Add files via upload

---
+++ 06. Reversed Chars/Program.cs
@@ -0,0 +1,5 @@
+﻿char firstSymbol = char.Parse(Console.ReadLine());
+char secondSymbol = char.Parse(Console.ReadLine());
+char thirdSymbol = char.Parse(Console.ReadLine());
+
+Console.WriteLine($"{thirdSymbol} {secondSymbol} {firstSymbol}");
