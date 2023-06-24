a308464d96c6328f33aa22446cac98fadfadf508
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-25 02:43:32 +0300

    Add files via upload

---
+++ 05. Special Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿int n = int.Parse(Console.ReadLine());
+bool flag;
+
+for (int num = 1; num <= n; num++)
+{
+    flag = false;
+    int sumOfDigits = 0;
+    int digits = num;
+
+    while (digits > 0)
+    {
+        sumOfDigits += digits % 10;
+        digits /= 10;
+    }
+
+    if (sumOfDigits == 5 || sumOfDigits == 7 || sumOfDigits == 11)
+    {
+        flag = true;
+    }
+
+    Console.WriteLine($"{num} -> {flag}");
+}
