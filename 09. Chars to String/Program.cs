f9a5af43c75ae1fe9612bfd46d28fff6b93322e1
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-27 01:47:11 +0300

    Add files via upload

---
+++ 09. Chars to String/Program.cs
@@ -0,0 +1,9 @@
+﻿char firstSymbol = char.Parse(Console.ReadLine());
+char secondSymbol = char.Parse(Console.ReadLine());
+char thirdSymbol = char.Parse(Console.ReadLine());
+
+string firstSymbolString = firstSymbol.ToString();
+string secondSymbolString = secondSymbol.ToString();
+string thirdSymbolString = thirdSymbol.ToString();
+
+Console.WriteLine($"{firstSymbolString}{secondSymbolString}{thirdSymbolString}");
