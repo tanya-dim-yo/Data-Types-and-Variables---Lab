ef61a2fa4129b11a13fad03b78bd86d4af0368e5
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-27 01:48:02 +0300

    Add files via upload

---
+++ 10. Lower or Upper/Program.cs
@@ -0,0 +1,11 @@
+﻿char symbol = char.Parse(Console.ReadLine());
+bool IsUpper = Char.IsUpper(symbol);
+
+if (IsUpper)
+{
+    Console.WriteLine("upper-case");
+}
+else
+{
+    Console.WriteLine("lower-case");
+}
