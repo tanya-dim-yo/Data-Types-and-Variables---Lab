c94bd5536dc06ac397cadb0654a358433b0b5473
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-27 01:46:31 +0300

    Add files via upload

---
+++ 08. Town Info/Program.cs
@@ -0,0 +1,5 @@
+﻿string townName = Console.ReadLine();
+int population = int.Parse(Console.ReadLine());
+int area = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"Town {townName} has population of {population} and area {area} square km.");
