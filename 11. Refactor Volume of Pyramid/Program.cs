004613fe887295b3169a64e6a49bbbe6a4898f8c
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-28 04:54:13 +0300

    Add files via upload

---
+++ 11. Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Length: ");
+double length = double.Parse(Console.ReadLine());
+Console.Write("Width: ");
+double width = double.Parse(Console.ReadLine());
+Console.Write("Height: ");
+double height = double.Parse(Console.ReadLine());
+double pyramidVolume = (length * width * height) / 3.0;
+Console.Write($"Pyramid Volume: {pyramidVolume:f2}");
