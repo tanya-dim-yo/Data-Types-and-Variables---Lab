7280b9b35015b03a9be3e9021019d69330e429b9
Tanya Yordanova <110428464+tanya-dim-yo@users.noreply.github.com>
2023-06-28 04:54:59 +0300

    Add files via upload

---
+++ 12. Refactor Special Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿int number = int.Parse(Console.ReadLine());
+bool isSpecial;
+
+for (int currentNumber = 1; currentNumber <= number; currentNumber++)
+{
+    int numberCopy = currentNumber;
+    int sum = 0;
+    isSpecial = false;
+
+    while (numberCopy > 0)
+    {
+        sum += numberCopy % 10;
+        numberCopy /= 10;
+    }
+
+    if (sum == 5 || sum == 7 || sum == 11)
+    {
+        isSpecial = true;
+    }
+    
+    Console.WriteLine($"{currentNumber} -> {isSpecial}");
+}
